5b591e792aa98862709da1c60850ae58aab98056
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-05-06 09:05:05 +0100

    Updated with initial fluent builder addtion

---
+++ Builder/test/BuilderTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BuilderPattern;
+
+namespace BuilderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var person = new PersonBuilder()
+                .Create("Gary", "Woodfine")
+                .Gender(Gender.Male)
+                .DateOfBirth(DateTime.Now)
+                .Occupation("Freelance Full-Stack Developer")
+                .Build();
+            
+            
+            Console.WriteLine(person.ToString());
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
