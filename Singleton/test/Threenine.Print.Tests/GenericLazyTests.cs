26e9a8851bd838d3deb20619fbe879f10cfddb4d
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-11-06 09:26:34 +0000

    Added Unit Tests

---
+++ Singleton/test/Threenine.Print.Tests/GenericLazyTests.cs
@@ -0,0 +1,34 @@
+using System;
+using Threenine.Print.Simple;
+using Xunit;
+
+namespace Threenine.Print.Tests
+{
+    public class GenericLazyTests
+    {
+        [Fact]
+        public void ShouldCreateOneInstance()
+        {
+            var firstItem = new PrintQueueItem {DocumentName = "firstItem"};
+            var secondItem = new PrintQueueItem {DocumentName = "secondItem"};
+            var instance1 = Spooler.Instance;
+            var instance2 = Spooler.Instance;
+            
+            Assert.Same(instance1, instance2);
+            
+            instance1.Queue.Add(firstItem);
+            
+            Assert.Equal(instance1.Queue.Count, instance2.Queue.Count);
+            
+            instance2.Queue.Add(secondItem);
+            
+            Assert.Equal(instance1.Queue.Count, instance2.Queue.Count);
+            
+            Assert.Same(instance1, instance2);
+
+            
+            
+
+        }
+    }
+}
