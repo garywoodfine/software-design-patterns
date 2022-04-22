8ab7b2f286f7a1b0eb36e2a6ef20641c2412e05b
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-04-22 08:45:08 +0100

    moving to office

--- strategy/StrategyClient/Rules/NotInRangeRule.cs
+++ strategy/StrategyClient/Rules/NotInRangeRule.cs
@@ -1,9 +1,16 @@
+using System.Linq;
+
 namespace StrategyClient.Rules
 {
     public class NotInRangeRule : IRule
     {
         public bool Verify(string choice)
         {
+            if (choice.All(char.IsDigit))
+            {
+                
+            }
+                
             return false;
         }
 
